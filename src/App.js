c7c0aeec2237df861e0c2cb000f27adc07107817
Beregovich <beregovich@mail.ru>
2022-01-16 22:38:08 +0300

    busido-11 grid

--- src/App.js
+++ src/App.js
@@ -2,8 +2,41 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="app-wrapper">
+      <header className='header'>Header      </header>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src='https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' />
+        </div>
+        <div>
+          ava+desc
+        </div>
+        <div>
+          my posts
+          <div>
+            new post
+          </div>
+          <div> post 1</div>
+          <div> post 2</div>
+        </div>
+      </div>
     </div>
   );
 }
