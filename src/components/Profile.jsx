723e8705d1c55bea95da42b3c3f947344e0dee8b
Beregovich <beregovich@mail.ru>
2022-01-16 23:15:13 +0300

    busido-12 breaking into components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Profile() {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' />
+            </div>
+            <div>
+                ava+desc
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+                <div> post 1</div>
+                <div> post 2</div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
