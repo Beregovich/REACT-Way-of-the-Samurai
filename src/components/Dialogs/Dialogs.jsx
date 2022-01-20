08f8b83c3c434d929de309609612873f7dead1c6
Beregovich <beregovich@mail.ru>
2022-01-20 16:28:10 +0300

    busido-21. Layout dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,7 +5,28 @@ import classes from './Dialogs.module.css';
 const Dialogs = (props) => {
     return (
         <div className={classes.dialogs}>
-            <img src='https://cs9.pikabu.ru/post_img/big/2017/03/18/5/14898230801910566.jpg' />
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog}>
+                    Vasya
+                </div>
+                <div className={classes.dialog}>
+                    Petya
+                </div>
+                <div className={classes.dialog}>
+                    Kolya
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>
+                    Some first message
+                </div>
+                <div className={classes.message}>
+                    Some second message
+                </div>
+                <div className={classes.message}>
+                    Some third message
+                </div>
+            </div>
         </div>
     )
 }
