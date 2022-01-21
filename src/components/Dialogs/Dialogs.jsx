d6b921b2222796eba566a9c0c8a7f5c69e1811c4
Beregovich <beregovich@mail.ru>
2022-01-21 13:44:17 +0300

    busido-24 Add BLL as arrays for Messages, Dialogs, Posts

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import classes from './Dialogs.module.css';
 import { NavLink } from 'react-router-dom';
 
+let messagesData = [
+    { id: 1, message: "First" },
+    { id: 2, message: "Second" },
+    { id: 3, message: "Third" },
+]
+
+let dialogsData = [
+    { id: 1, name: "Vasya" },
+    { id: 2, name: "Petya" },
+    { id: 3, name: "Kolya" },
+    { id: 4, name: "Serezha" },
+]
+
 const DialogItem = (props) => {
     let path = "/dialogs/" + props.id;
     return (
@@ -12,6 +25,8 @@ const DialogItem = (props) => {
 }
 
 const Message = (props) => {
+
+
     return (
         <div className={classes.message}>
             {props.message}
@@ -23,15 +38,14 @@ const Dialogs = (props) => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <DialogItem id='1' name='Vasya' />
-                <DialogItem id='2' name='Petya' />
-                <DialogItem id='3' name='Kolya' />
-                <DialogItem id='4' name='Serezha' />
+                <DialogItem id={dialogsData[0].id} name={dialogsData[1].name} />
+                <DialogItem id={dialogsData[1].id} name={dialogsData[2].name} />
+                <DialogItem id={dialogsData[2].id} name={dialogsData[3].name} />
             </div>
             <div className={classes.messages}>
-                <Message message="First" />
-                <Message message="Secondt" />
-                <Message message="Third" />
+                <Message message={messagesData[0].message} />
+                <Message message={messagesData[1].message} />
+                <Message message={messagesData[2].message} />
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
+let postsData = [
+    { id: 1, name: "Vasya", message: "I'am Vasya, Hi!" },
+    { id: 2, name: "Petya", message: "I'am Petya, Hi!" },
+    { id: 3, name: "Kolya", message: "I'am Kolya, Hi!" },
+]
+
 const MyPosts = () => {
     return (
         <div className={classes.posts}>
-            <Post name="Vasya" message="I'am Vasya, Hi!" />
-            <Post name="Petya" message="I'am Petya, Hi!" />
-            <Post name="Kolya" message="I'am Kolya, Hi!" />
+            <Post name={postsData[0].name} message={postsData[0].message} />
+            <Post name={postsData[1].name} message={postsData[0].message} />
+            <Post name={postsData[2].name} message={postsData[0].message} />
+
         </div>
 
 
