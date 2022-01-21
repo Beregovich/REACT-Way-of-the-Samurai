f87cd5709c344998918e5e4c6a766efb8d2f1851
Beregovich <beregovich@mail.ru>
2022-01-21 17:44:20 +0300

    busido-26 Extract Message and DialogItem components from Dialogs component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
-import { NavLink } from 'react-router-dom';
-
-let messages = [
-    { id: 1, message: "First" },
-    { id: 2, message: "Second" },
-    { id: 3, message: "Third" },
-]
+import DialogItem from './DialogItem/DialogItem';
+import Message from './Message/Message'
 
 let dialogs = [
     { id: 1, name: "Vasya" },
@@ -15,43 +10,30 @@ let dialogs = [
     { id: 4, name: "Serezha" },
 ]
 
-let messagesElements = messages.map(
-    m => <DialogItem message={m.name} />
-)
+let messages = [
+    { id: 1, message: "First" },
+    { id: 2, message: "Second" },
+    { id: 3, message: "Third" },
+]
 
 let dialogsElements = dialogs.map(
     d => <DialogItem id={d.id} name={d.name} />
 )
 
-const DialogItem = (props) => {
-    let path = "/dialogs/" + props.id;
-    return (
-        <div className={classes.dialog}>
-            <NavLink to={path}>{props.name}</NavLink>
-        </div>
-    )
-}
 
-const Message = (props) => {
-
-
-    return (
-        <div className={classes.message}>
-            {props.message}
-        </div>
-    )
-}
+let messagesElements = messages.map(
+    m => <Message id={m.id} message={m.message} />
+)
 
 const Dialogs = (props) => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
                 {dialogsElements}
-
             </div>
             <div className={classes.messages}>
                 {messagesElements}
-
+                <Message id='2' message='mesg' />
             </div>
         </div>
     )
