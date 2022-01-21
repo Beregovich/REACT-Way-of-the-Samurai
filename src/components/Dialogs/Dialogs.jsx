d9ec89ad126dffc706d6deb78150177c4206844a
Beregovich <beregovich@mail.ru>
2022-01-21 12:53:44 +0300

    busido-23. Add ProfileInfo component, DialogsItem, Messages components (inside Dialogs.jsx)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,30 +2,36 @@ import React from 'react';
 import classes from './Dialogs.module.css';
 import { NavLink } from 'react-router-dom';
 
+const DialogItem = (props) => {
+    let path = "/dialogs/" + props.id;
+    return (
+        <div className={classes.dialog}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+const Message = (props) => {
+    return (
+        <div className={classes.message}>
+            {props.message}
+        </div>
+    )
+}
+
 const Dialogs = (props) => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <div className={classes.dialog}>
-                    <NavLink to='/dialogs/1'>Vasya</NavLink>
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/dialogs/2'>Petya</NavLink>
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/dialogs/3'>Kolya</NavLink>
-                </div>
+                <DialogItem id='1' name='Vasya' />
+                <DialogItem id='2' name='Petya' />
+                <DialogItem id='3' name='Kolya' />
+                <DialogItem id='4' name='Serezha' />
             </div>
             <div className={classes.messages}>
-                <div className={classes.message}>
-                    Some first message
-                </div>
-                <div className={classes.message}>
-                    Some second message
-                </div>
-                <div className={classes.message}>
-                    Some third message
-                </div>
+                <Message message="First" />
+                <Message message="Secondt" />
+                <Message message="Third" />
             </div>
         </div>
     )
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import classes from './Content.module.css';
 import MyPosts from './MyPosts/MyPosts';
+import ProfileInfo from './ProfileInfo/ProdileInfo';
 
 function Profile() {
     return (
         <div className={classes.content}>
-            <div>
-                <img src='https://images.unsplash.com/photo-1504194104404-433180773017?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' />
-            </div>
-            <div>
-                Some description  or status;
-            </div>
+            <ProfileInfo />
             <MyPosts />
         </div>
 
