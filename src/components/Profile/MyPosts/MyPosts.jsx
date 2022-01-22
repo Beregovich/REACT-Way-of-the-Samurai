fdb7c1b040872aa2a84cda205203374936b46537
Beregovich <beregovich@mail.ru>
2022-01-22 13:39:40 +0300

    Add textarea and button in MyPosts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,10 +7,22 @@ const MyPosts = (props) => {
     let postsElements = props.posts.map(
         (p) => <Post name={p.name} message={p.message} />
     )
-    return (
-        <div className={classes.posts}>
-            {postsElements}
 
+
+    return (
+        <div className={classes.postBlock}>
+            <h3>My Posts</h3>
+            <div>
+                <textarea>
+
+                </textarea>
+            </div>
+            <div>
+                <button>Add Post</button>
+            </div>
+            <div className={classes.posts}>
+                {postsElements}
+            </div>
 
         </div>
 
