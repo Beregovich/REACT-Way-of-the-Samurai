347740ccc7679cc4f28a6f0caaab6e34e9ce698e
Beregovich <beregovich@mail.ru>
2022-01-27 21:12:16 +0300

    busido-49 add set-users in user-reducer

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,22 +1,23 @@
 const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW'
+const SET_USERS = 'SET-USERS'
 
 let initialState = {
     users: [
-        {
-            id: 1, fuulname: "Ivan!", status: 'Hello World!', followed: true,
-            location: {
-                country: 'Russis',
-                city: 'Voronezh'
-            }
-        },
-        {
-            id: 2, fuulname: "Petr!", status: 'Hello JS!', followed: false,
-            location: {
-                country: 'Russis',
-                city: 'Novohopersk'
-            }
-        },
+        /* {
+             id: 1, fuulname: "Ivan!", status: 'Hello World!', followed: true,
+             location: {
+                 country: 'Russis',
+                 city: 'Voronezh'
+             }
+         },
+         {
+             id: 2, fuulname: "Petr!", status: 'Hello JS!', followed: false,
+             location: {
+                 country: 'Russis',
+                 city: 'Novohopersk'
+             }
+         },*/
     ],
 };
 
@@ -41,9 +42,15 @@ export const usersReducer = (state = initialState, action) => {
                 })
 
             }
+        case SET_USERS:
+            return {
+                ...state,
+                users: [...state.users, ...action.users]
+            }
         default: return state
     }
 }
 
 export const followAC = (userId) => ({ type: FOLLOW, userId });
 export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId });
+export const setUsersAC = (users) => ({ type: SET_USERS, users })
