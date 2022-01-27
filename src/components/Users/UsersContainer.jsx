e0dccb7690de876f94499f119ccbcfed932003db
Beregovich <beregovich@mail.ru>
2022-01-27 21:31:01 +0300

    busido-49 use connect in UsersContainer. Add mapStateProps and mapDispatchToProps functions

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import Users from './Users';
-const UsersContainer = (props) => {
+import { connect } from 'react-redux';
+import { followAC, unfollowAC, setUsersAC } from '../../redux/users-reducer';
 
-    return (
-        <Users />
-    )
+let mapDispatchToProps = (dispatch) => {
+    return {
+        follow: (userId) => {
+            dispatch(followAC(userId))
+        },
+        unfollow: (userId) => {
+            dispatch(unfollowAC(userId))
+        },
+        setUsers: (users) => {
+            setUsersAC(users)
+        }
+
+    }
 }
-export default UsersContainer;
+
+let mapStateToProps = (state) => {
+    return {
+        users: state.usersPage.users,
+    }
+}
+
+const UserContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+export default UserContainer;
