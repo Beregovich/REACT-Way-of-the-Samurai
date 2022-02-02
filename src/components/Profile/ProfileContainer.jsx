d9bd8179241886992be23fc48c08a62c4ff804c0
Beregovich <beregovich@mail.ru>
2022-02-02 21:54:51 +0300

    transform ProfileContainer component from class component to functional component

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -8,7 +8,7 @@ import { useParams } from 'react-router-dom'
 
 
 let ProfileContainer = (props) => {
-    //let { userId } = useParams();
+
     useEffect(() => {
         axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`).then(response => {
             props.setUserProfile(response.data)
@@ -17,19 +17,6 @@ let ProfileContainer = (props) => {
     )
     return <Profile {...props} profile={props.profile} />
 }
-/*class ProfileContainer extends React.Component {
-
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`).then(response => {
-            this.props.setUserProfile(response.data)
-        })
-
-    }
-
-    render() {
-        return <Profile {...this.props} profile={this.props.profile} />
-    }
-}*/
 
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile
