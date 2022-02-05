4b696439aeca42759c5ca2d7b62da3e71b91e24b
Beregovich <beregovich@mail.ru>
2022-02-05 23:19:10 +0300

    busido-73 get and update status on server. http put, useState,

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import Profile from './Profile'
 import { connect } from 'react-redux';
 import { getUserProfile } from '../../redux/profile-reducer'
+import { getUserStatus } from '../../redux/profile-reducer'
+import { updateUserStatus } from '../../redux/profile-reducer'
 import { useParams } from 'react-router-dom'
 import { withAuthRedirect } from '../../hoc/withAuthRedirect'
 
@@ -13,20 +15,22 @@ let ProfileContainer = (props) => {
         }
         else {
             props.getUserProfile(userId)
+            props.getUserStatus(userId)
         }
 
     }, [])
 
-    return <Profile {...props} profile={props.profile} />
+    return <Profile {...props} profile={props.profile} status={props.status} updateUserStatus={props.updateUserStatus} />
 }
 
 //let AuthRedirectComponent = withAuthRedirect(ProfileContainer)
 
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
-    isAuth: state.auth.isAuth
+    isAuth: state.auth.isAuth,
+    status: state.profilePage.status
 })
-export default connect(mapStateToProps, { getUserProfile })(ProfileContainer)
+export default connect(mapStateToProps, { getUserProfile, getUserStatus, updateUserStatus })(ProfileContainer)
 
 
 
--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -2,20 +2,30 @@ import React, { useState } from 'react';
 
 function ProfileStatus(props) {
     let [editMode, setEditMode] = useState(false);
-    let toggleEditMode = () => {
-        setEditMode(!editMode)
-    }
-    return <div>
-        {editMode
-            ? <div>
-                <span onDoubleClick={toggleEditMode}>{props.profile.userId}</span>
-            </div>
-            : <div>
-                <input autoFocus={true} value={props.profile.userId} onBlur={toggleEditMode} />
-            </div>
-        }
+    let [status, setStatus] = useState(props.status)
 
-    </div>;
+
+    let activateEditMode = () => {
+        setEditMode(true)
+        props.updateUserStatus(status)
+    }
+    let deactivateEditMode = () => {
+        setEditMode(false)
+        props.updateUserStatus(status)
+    }
+
+    let updateStatus = (e) => {
+        setStatus(e.currentTarget.value)
+    }
+    return (
+        <div>
+            {!editMode ? <div>
+                <span onDoubleClick={activateEditMode}>{props.status}</span>
+            </div>
+                : <div>
+                    <input onChange={updateStatus} autoFocus={true} defaultValue={props.status} onBlur={deactivateEditMode} />
+                </div>}
+        </div>)
 }
 
 export default ProfileStatus;
